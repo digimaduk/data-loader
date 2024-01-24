3181f4ecf034531e77aadd56635f6a06a9a1c9ac
digimaduk <meghadureja@192.168.1.119>
2024-01-24 22:58:53 +0000

    fix CORS

--- src/main/java/org/digimad/dataloader/controller/DataLoader.java
+++ src/main/java/org/digimad/dataloader/controller/DataLoader.java
@@ -4,14 +4,13 @@ import org.digimad.dataloader.dto.Table;
 import org.digimad.dataloader.entities.OrderItem;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class DataLoader {
 
 //    @GetMapping("/{orderId}/orderitems")
@@ -44,6 +43,11 @@ public class DataLoader {
         list.add(order);
         list.add(orderItem);
         return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @PostMapping("/tables-data")
+    public ResponseEntity<List<Table>> getTablesData() {
+        return null;
     }
 
 }
